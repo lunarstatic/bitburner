d71e3772c188efd7bc163c805d6d49be4c557408
lunar::static <96537117+lunarstatic@users.noreply.github.com>
2021-12-31 05:04:00 -0700

    Update x2.js

--- x2.js
+++ x2.js
@@ -3,29 +3,42 @@
 // Infinite loop that continously hacks/grows/weakens the target server
 
 export async function main(ns) {
-var target = "silver-helix";
+    var target = "silver-helix";
+    var moneyThresh = ns.getServerMaxMoney(target) * 0.75;
+    var securityThresh = ns.getServerMinSecurityLevel(target) + 5;
 
-// Defines how much money a server should have before we hack it
-// 75% of the server's max money
-var moneyThresh = ns.getServerMaxMoney(target) * 0.75;
 
-// Defines the maximum security level the target server can
-// have. If the target's security level is higher than this,
-// we'll weaken it before doing anything else
-var securityThresh = ns.getServerMinSecurityLevel(target) + 5;
+    if (ns.fileExists("BruteSSH.exe")) {
+        ns.brutessh(target);
+    }
+    if (ns.fileExists("FTPCrack.exe")) {
+        ns.ftpcrack(target);
+    }
+    if (ns.fileExists("HTTPWorm.exe")) {
+        ns.httpworm(target);
+    }
+    if (ns.fileExists("relaySMTP.exe")) {
+        ns.relaysmtp(target);
+    }
+    if (ns.fileExists("SQLInject.exe")) {
+        ns.sqlinject(target);
+    }
 
-	while (true) {
-		if (ns.getServerSecurityLevel(target) > securityThresh) {
-			// If the server's security level is above our threshold, weaken it
-			await ns.weaken(target);
-		} else if (ns.getServerMoneyAvailable(target) < moneyThresh) {
-			// If the server's money is less than our threshold, grow it
-			await ns.grow(target);
-		} else {
-			// Otherwise, hack it
-			await ns.hack(target);
-		}
-	}
+    ns.nuke(target);
 
+
+
+    while (ns.hasRootAccess(target)) {
+        if (ns.getServerSecurityLevel(target) > securityThresh) {
+            // If the server's security level is above our threshold, weaken it
+            await ns.weaken(target);
+        } else if (ns.getServerMoneyAvailable(target) < moneyThresh) {
+            // If the server's money is less than our threshold, grow it
+            await ns.grow(target);
+        } else {
+            // Otherwise, hack it
+            await ns.hack(target);
+        }
+    }
 
 }
