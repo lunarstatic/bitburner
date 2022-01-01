418b684bc76ee725ca891e8cad3a1b38547255bb
lunar::static <96537117+lunarstatic@users.noreply.github.com>
2022-01-01 03:27:48 -0700

    Update x5.js

--- x5.js
+++ x5.js
@@ -1,44 +1,53 @@
 /** @param {NS} ns **/
 
-// Infinite loop that continously hacks/grows/weakens the target server
 
 export async function main(ns) {
-    var target = "harakiri-sushi";
-    var moneyThresh = ns.getServerMaxMoney(target) * 0.75;
-    var securityThresh = ns.getServerMinSecurityLevel(target) + 5;
 
 
-    if (ns.fileExists("BruteSSH.exe")) {
-        ns.brutessh(target);
+    var tservs = ns.read("targs.txt").split('",\r\n"');
+    for (var i = 6; i < tservs.length - 2; ++i) {
+
+        var target = tservs[i].split('",');
+        var moneyThresh = ns.getServerMaxMoney(target) * 0.75;
+        var securityThresh = ns.getServerMinSecurityLevel(target) + 5;
+
+
+        if (ns.fileExists("BruteSSH.exe")) {
+            ns.brutessh(target);
+        }
+        if (ns.fileExists("FTPCrack.exe")) {
+            ns.ftpcrack(target);
+        }
+        if (ns.fileExists("HTTPWorm.exe")) {
+            ns.httpworm(target);
+        }
+        if (ns.fileExists("relaySMTP.exe")) {
+            ns.relaysmtp(target);
+        }
+        if (ns.fileExists("SQLInject.exe")) {
+            ns.sqlinject(target);
+        }
+
+        ns.nuke(target);
     }
-    if (ns.fileExists("FTPCrack.exe")) {
-        ns.ftpcrack(target);
-    }
-    if (ns.fileExists("HTTPWorm.exe")) {
-        ns.httpworm(target);
-    }
-    if (ns.fileExists("relaySMTP.exe")) {
-        ns.relaysmtp(target);
-    }
-    if (ns.fileExists("SQLInject.exe")) {
-        ns.sqlinject(target);
-    }
 
-    ns.nuke(target);
+    var tservs = ns.read("targs.txt").split('",\r\n"');
+    for (var i = 6; i < tservs.length - 2; ++i) {
+        var target = tservs[i].split('",');
+        while (ns.hasRootAccess(target)) {
+            if (ns.getServerSecurityLevel(target) > securityThresh) {
+                // If the server's security level is above our threshold, weaken it
 
+                await ns.weaken(target);
+            } else if (ns.getServerMoneyAvailable(target) < moneyThresh) {
+                // If the server's money is less than our threshold, grow it
 
+                await ns.grow(target);
+            } else {
+                // Otherwise, hack it
 
-    while (ns.hasRootAccess(target)) {
-        if (ns.getServerSecurityLevel(target) > securityThresh) {
-            // If the server's security level is above our threshold, weaken it
-            await ns.weaken(target);
-        } else if (ns.getServerMoneyAvailable(target) < moneyThresh) {
-            // If the server's money is less than our threshold, grow it
-            await ns.grow(target);
-        } else {
-            // Otherwise, hack it
-            await ns.hack(target);
+                await ns.hack(target);
+            }
         }
     }
-
 }
