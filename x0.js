72676d24719e80c0e25da3bdb0cb017989bf2482
lunar::static <96537117+lunarstatic@users.noreply.github.com>
2022-01-02 06:00:39 -0700

    Update x0.js

--- x0.js
+++ x0.js
@@ -2,41 +2,12 @@
 
 
 export async function main(ns) {
-
-
-    var hacklvl = ns.getHackingLevel();
     var tservs = ns.read("targs.txt").split('",\r\n"');
     for (var i = 1; i < tservs.length - 7; ++i) {
 
         var target = tservs[i].split('",');
         var moneyThresh = ns.getServerMaxMoney(target) * 0.75;
         var securityThresh = ns.getServerMinSecurityLevel(target) + 5;
-
-        if (!ns.hasRootAccess(target) && ns.getServerRequiredHackingLevel(target) < hacklvl) {
-            if (ns.fileExists("BruteSSH.exe")) {
-                ns.brutessh(target);
-            }
-            if (ns.fileExists("FTPCrack.exe")) {
-                ns.ftpcrack(target);
-            }
-            if (ns.fileExists("HTTPWorm.exe")) {
-                ns.httpworm(target);
-            }
-            if (ns.fileExists("relaySMTP.exe")) {
-                ns.relaysmtp(target);
-            }
-            if (ns.fileExists("SQLInject.exe")) {
-                ns.sqlinject(target);
-            }
-            if (!ns.hasRootAccess(target) && ns.getServerRequiredHackingLevel(target) < hacklvl) {
-                ns.nuke(target);
-            }
-        }
-    }
-
-    var tservs = ns.read("targs.txt").split('",\r\n"');
-    for (var i = 1; i < tservs.length - 7; ++i) {
-        var target = tservs[i].split('",');
 
         while (ns.hasRootAccess(target)) {
             if (ns.getServerSecurityLevel(target) > securityThresh) {
