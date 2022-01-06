27a93546240ebc46e0bc987242608be48bb24d0a
lunar::static <96537117+lunarstatic@users.noreply.github.com>
2022-01-06 12:41:17 -0700

    Create update.js

---
+++ update.js
@@ -0,0 +1,12 @@
+/** @param {NS} ns **/
+export async function main(ns) {
+ns.wget("https://github.com/lunarstatic/bitburner/blob/main/bl173.js", "bl173.js");
+ns.wget("https://github.com/lunarstatic/bitburner/blob/main/x.js", "x.js");
+ns.wget("https://github.com/lunarstatic/bitburner/blob/main/earlystart.js", "earlystart.js");
+ns.wget("https://github.com/lunarstatic/bitburner/blob/main/1337.js", "1337.js");
+ns.wget("https://github.com/lunarstatic/bitburner/blob/main/targs.txt", "targs.txt");
+ns.wget("https://github.com/lunarstatic/bitburner/blob/main/targs2.txt", "targs2.txt");
+ns.wget("https://github.com/lunarstatic/bitburner/blob/main/servs.txt", "servs.txt");
+
+
+}
