007c97acbb7935f5517dc5c6337fe5f818fa6a35
lunar::static <96537117+lunarstatic@users.noreply.github.com>
2022-01-08 05:52:46 -0700

    Create analyze.js

---
+++ analyze.js
@@ -0,0 +1,30 @@
+var target = "ecorp"
+var hackamt = 1000000000000;
+var weaktime = ns.getWeakenTime(target);
+var growtime = ns.getGrowTime(target);
+var hacktime = ns.getHackTime(target);
+var threads = ns.hackAnalyzeThreads(target, hackamt);
+
+ns.tprint("Threads needed to hack 1t from : " + target + " " + ns.nFormat(ns.hackAnalyzeThreads(target, hackamt), '0,0'));
+ns.tprint("Time needed to weaken : " + target + " " + ns.tFormat(ns.getWeakenTime(target), '0,0'));
+ns.tprint("Time needed to grow : " + target + " " + ns.tFormat(ns.getGrowTime(target), '0,0'));
+ns.tprint("Time needed to hack : " + target + " " + ns.tFormat(ns.getHackTime(target), '0,0'));
+
+
+	var moneyThresh = ns.getServerMaxMoney(target) * 0.75;
+	var securityThresh = ns.getServerMinSecurityLevel(target) + 5;
+
+	while (ns.hasRootAccess(target)) {
+
+		if (ns.getServerSecurityLevel(target) > securityThresh) {
+
+			await ns.weaken(target);
+		} else if (ns.getServerMoneyAvailable(target) < moneyThresh) {
+
+			await ns.grow(target);
+		} else {
+
+			await ns.hack(target);
+		}
+
+	}
