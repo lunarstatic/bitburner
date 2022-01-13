853b54d4d260f3d4e9f685262c683b11c408b581
lunar::static <96537117+lunarstatic@users.noreply.github.com>
2022-01-13 00:50:27 -0700

    Update st.js

--- st.js
+++ st.js
@@ -3,19 +3,108 @@ export async function main(ns) {
 	ns.tail("st.js");
 
 	var servs = [
-		"n00dles",
 		"foodnstuff",
 		"sigma-cosmetics",
 		"joesguns",
+		"nectar-net",
 		"hong-fang-tea",
 		"harakiri-sushi",
+		"neo-net",
+		"zer0",
+		"max-hardware",
+		"iron-gym",
+		"phantasy",
+		"omega-net",
+		"the-hub",
+//		"lexo-corp",
+//		"microdyne",
+//		"unitalife",
+//		"silver-helix",
+//		"rho-construction",
+//		"netlink",
+//		"aevum-police",
+//		"summit-uni",
+//		"alpha-ent",
+//		"solaris",
+//		"run4theh111z",
+//		"millenium-fitness",
+//		"catalyst",
+//		"omnia",
+//		".",
+		"avmnite-02h",
+//		"titan-labs",
+//		"zb-institute",
+//		"rothman-uni",
+//		"univ-energy",
+		"I.I.I.I",
+//		"global-pharm",
+//		"omnitek",
+//		"blade",
+//		"fulcrumtech",
+//		"helios",
+//		"stormtech",
+//		"ecorp",
+//		"megacorp",
+//		"nwo",
+//		"kuai-gong",
+//		"deltaone",
+//		"zeus-med",
+//		"zb-def",
+//		"infocomm",
+//		"taiyang-digital",
 	]
+
 	var home = "home";
 	var targ = "foodnstuff";
 	var i = 0;
 	var f = 0;
+	var hacklvl = ns.getHackingLevel();
 
+	//count how many exes we have
+	var execount = 0;
+	if (ns.fileExists("BruteSSH.exe")) {
+		++execount
+	}
+	if (ns.fileExists("FTPCrack.exe")) {
+		++execount
+	}
+	if (ns.fileExists("HTTPWorm.exe")) {
+		++execount
+	}
+	if (ns.fileExists("relaySMTP.exe")) {
+		++execount
+	}
+	if (ns.fileExists("SQLInject.exe")) {
+		++execount
+	}
 
+	ns.tprint("You have " + execount + " port openers."); //How many exe's do I have?
+
+	for (var j = 0; j < servs.length; ++j) {
+		var serv = servs[j].split('",');
+		if (!ns.hasRootAccess(serv) && ns.getServerRequiredHackingLevel(serv) <= hacklvl) { //make sure you can hack this server if you haven't already
+
+			if (ns.fileExists("BruteSSH.exe")) {
+				ns.brutessh(serv);
+			} else (ns.tprint("BruteSSH.exe does not exist."));
+			if (ns.fileExists("FTPCrack.exe")) {
+				ns.ftpcrack(serv);
+			} else (ns.tprint("FTPCrack.exe does not exist."));
+			if (ns.fileExists("HTTPWorm.exe")) {
+				ns.httpworm(serv);
+			} else (ns.tprint("HTTPWorm.exe does not exist."));
+			if (ns.fileExists("relaySMTP.exe")) {
+				ns.relaysmtp(serv);
+			} else (ns.tprint("relaySMTP.exe does not exist."));
+			if (ns.fileExists("SQLInject.exe")) {
+				ns.sqlinject(serv);
+			} else (ns.tprint("SQLInject.exe does not exist."));
+
+			if (ns.getServerNumPortsRequired(serv) <= execount) {
+				ns.nuke(serv);
+			} else (ns.tprint("Cannot NUKE " + targ + " : Cannot open the needed # of ports."))
+		}
+	}
 
 	//kill script then copy current version from home to each serv
 	for (i = 0; i < servs.length; ++i) {
